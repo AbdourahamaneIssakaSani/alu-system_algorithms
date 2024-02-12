4b54cc70f456d12dc52d3d1647c8143e4bd04057
Abissa <67793634+AbdourahamaneIssakaSani@users.noreply.github.com>
2024-02-12 08:09:29 -0800

    :robot: automated commit of 2-graph_add_edge.c

--- graphs/2-graph_add_edge.c
+++ graphs/2-graph_add_edge.c
@@ -1,10 +1,10 @@
 
 #include "graphs.h"
 
-
 vertex_t *get_vertex(graph_t *graph, const char *str);
+
 edge_t *create_edge(vertex_t *src_vertex,
-vertex_t *dest_vertex, edge_type_t type);
+					vertex_t *dest_vertex, edge_type_t type);
 
 /**
 * graph_add_edge - adds an edge between two vertices to the graph
@@ -55,7 +55,6 @@ int graph_add_edge(graph_t *graph, const char *src,
 	return (1);
 }
 
-
 /**
 * get_vertex - get a vertex from the graph
 * @graph: pointer to graph type
@@ -92,11 +91,9 @@ vertex_t *get_vertex(graph_t *graph, const char *str)
 * Return: pointer to the new edge
 */
 edge_t *create_edge(vertex_t *src_vertex,
-vertex_t *dest_vertex, edge_type_t type)
+					vertex_t *dest_vertex, edge_type_t type)
 {
-	edge_t *edge;
-
-	edge = malloc(sizeof(edge_t));
+	edge_t *edge = malloc(sizeof(edge_t));
 
 	if (edge == NULL)
 	{
@@ -106,30 +103,24 @@ vertex_t *dest_vertex, edge_type_t type)
 	edge->dest = dest_vertex;
 	edge->next = NULL;
 
-	if (type == UNIDIRECTIONAL)
+	if (type == BIDIRECTIONAL)
 	{
-		edge->next = src_vertex->edges;
-		src_vertex->edges = edge;
-	}
-	else if (type == BIDIRECTIONAL)
-	{
-		edge->next = src_vertex->edges;
-		src_vertex->edges = edge;
 
-		edge = malloc(sizeof(edge_t));
+		edge_t *reverse_edge = malloc(sizeof(edge_t));
 
-		if (edge == NULL)
+		if (reverse_edge == NULL)
 		{
-			/* Clean up the allocated memory for the first edge */
-			free(src_vertex->edges);
+			free(edge);
 			return (NULL);
 		}
 
-		edge->dest = src_vertex;
-		edge->next = dest_vertex->edges;
-		dest_vertex->edges = edge;
+		reverse_edge->dest = src_vertex;
+		reverse_edge->next = dest_vertex->edges;
+		dest_vertex->edges = reverse_edge;
 	}
+
+	edge->next = src_vertex->edges;
+	src_vertex->edges = edge;
 
 	return (edge);
 }
-
