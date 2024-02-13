76384b0c4f449e6d51d9e751399252032d2a1b3b
Abissa <67793634+AbdourahamaneIssakaSani@users.noreply.github.com>
2024-02-13 09:27:52 -0800

    :robot: automated commit of heap_insert.c

--- huffman_coding/heap/heap_insert.c
+++ huffman_coding/heap/heap_insert.c
@@ -1,68 +1,87 @@
 #include "heap.h"
 
+
+void heap_insert_recursive(heap_t *heap,
+binary_tree_node_t *root, binary_tree_node_t *node);
+void heapify(heap_t *heap, binary_tree_node_t *node);
+
+
 /**
- * heap_insert - inserts new node into min heap
- * @heap: pointer to heap structure
- * @data: pointer to data
- * Return: pointer to new node or NULL
- */
+* heap_insert - inserts new node into min heap
+* @heap: pointer to heap structure
+* @data: pointer to data
+* Return: pointer to new node or NULL
+*/
 binary_tree_node_t *heap_insert(heap_t *heap, void *data)
 {
-    binary_tree_node_t *node = NULL, *parent = NULL;
-    void *tmp = NULL;
+	binary_tree_node_t *node = NULL;
 
-    if (!heap)
-        return (NULL);
+	if (!heap)
+		return (NULL);
 
-    node = binary_tree_node(NULL, data);
+	node = binary_tree_node(NULL, data);
 
-    if (!node)
-        return (NULL);
+	if (!node)
+		return (NULL);
 
-    if (!heap->root)
-    {
-        heap->root = node;
-    }
-    else
-    {
-        tmp = node->data;
-        parent = heap->root;
+	if (!heap->root)
+	{
+		heap->root = node;
+	}
+	else
+	{
+		heap_insert_recursive(heap, heap->root, node);
+		heapify(heap, node);
+	}
 
-        while (1)
-        {
-            if (parent->left == NULL)
-            {
-                parent->left = node, node->parent = parent;
-                break;
-            }
-            else if (parent->right == NULL)
-            {
-                parent->right = node, node->parent = parent;
-                break;
-            }
-            else if (heap->size % 2 == 0)
-            {
-                parent = parent->left;
-            }
-            else
-            {
-                parent = parent->right;
-            }
-            heap->size >>= 1;
-        }
+	heap->size++;
 
-        node->data = tmp;
+	return (node);
+}
 
-        while (node->parent && heap->data_cmp(node->data, node->parent->data) < 0)
-        {
-            tmp = node->parent->data;
-            node->parent->data = node->data;
-            node->data = tmp;
-            node = node->parent;
-        }
-    }
+/**
+* heap_insert_recursive - recursively inserts new node into min heap
+* @heap: pointer to heap structure
+* @root: current root
+* @node: pointer to node to insert
+*/
+void heap_insert_recursive(heap_t *heap,
+binary_tree_node_t *root, binary_tree_node_t *node)
+{
+	if (!root->left)
+	{
+		root->left = node;
+		node->parent = root;
+	}
+	else if (!root->right)
+	{
+		root->right = node;
+		node->parent = root;
+	}
+	else if (heap->size % 2 == 0)
+	{
+		heap_insert_recursive(heap, root->left, node);
+	}
+	else
+	{
+		heap_insert_recursive(heap, root->right, node);
+	}
+}
 
-    heap->size++;
+/**
+* heapify - maintains heap property after insertion
+* @heap: pointer to heap structure
+* @node: pointer to node to heapify
+*/
+void heapify(heap_t *heap, binary_tree_node_t *node)
+{
+	void *tmp = NULL;
 
-    return (node);
+	while (node->parent && heap->data_cmp(node->data, node->parent->data) < 0)
+	{
+		tmp = node->parent->data;
+		node->parent->data = node->data;
+		node->data = tmp;
+		node = node->parent;
+	}
 }
