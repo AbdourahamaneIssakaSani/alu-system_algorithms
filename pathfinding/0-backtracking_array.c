6a70f64796e5ba97c7017f330134f7ec77ae9e10
Abissa <67793634+AbdourahamaneIssakaSani@users.noreply.github.com>
2024-02-13 07:35:18 -0800

    :robot: automated commit of 0-backtracking_array.c

---
+++ pathfinding/0-backtracking_array.c
@@ -0,0 +1,71 @@
+#include "pathfinding.h"
+
+/**
+* backtracking_array - backtracking algorithm
+* @map: map
+* @rows: number of rows
+* @cols: number of columns
+* @start: start point
+* @target: target point
+* Return: queue of points
+*/
+queue_t *backtracking_array(char **map, int rows, int cols,
+
+							point_t const *start, point_t const *target)
+{
+	queue_t *path = queue_create();
+
+	if (!map || !*map || rows < 1 || cols < 1 || !start || !target || !path)
+		return (NULL);
+
+	if (backtrack(map, rows, cols, target, start->x, start->y, path))
+		return (path);
+
+	free(path);
+	return (NULL);
+}
+
+/**
+* backtrack - backtracking algorithm
+* @map: map
+* @rows: number of rows
+* @cols: number of columns
+* @target: target point
+* @x: current x
+* @y: current y
+* @path: path
+* Return: 1 on success, 0 on failure
+*/
+int backtrack(char **map, int rows, int cols, point_t const *target,
+
+			int x, int y, queue_t *path)
+{
+	if (x < 0 || x >= cols || y < 0 || y >= rows || map[y][x] == '1')
+		return (0);
+
+	point_t *point = malloc(sizeof(*point));
+
+	if (!point)
+		return (0);
+
+	point->x = x;
+	point->y = y;
+
+	if (enqueue(path, point))
+		return (0);
+
+	if (x == target->x && y == target->y)
+		return (1);
+
+	map[y][x] = '1';
+
+	if (backtrack(map, rows, cols, target, x + 1, y, path) ||
+		backtrack(map, rows, cols, target, x - 1, y, path) ||
+		backtrack(map, rows, cols, target, x, y + 1, path) ||
+		backtrack(map, rows, cols, target, x, y - 1, path))
+		return (1);
+
+	free(dequeue(path));
+
+	return (0);
+}
